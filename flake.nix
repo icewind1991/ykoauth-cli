71c255ac452f50e2b0dfbe7213b947940102e730
Robin Appelman <robin@icewind.nl>
2023-01-01 16:42:00 +0100

    flake package

--- flake.nix
+++ flake.nix
@@ -2,23 +2,34 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/release-22.05";
-    riff.url = "github:DeterminateSystems/riff";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
-    riff,
+    naersk,
   }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = (import nixpkgs) {
         inherit system;
       };
+      naersk-lib = naersk.lib."${system}";
+      buildDependencies = with pkgs; [gcc pkg-config pcsclite xorg.libX11.dev];
     in rec {
+     # `nix build`
+     packages.ykoauth-cli = naersk-lib.buildPackage {
+       pname = "ykoauth-cli";
+       root = ./.;
+       nativeBuildInputs = buildDependencies;
+     };
+     defaultPackage = packages.ykoauth-cli;
+     defaultApp = packages.ykoauth-cli;
+
       # `nix develop`
       devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [rustc cargo bacon cargo-edit cargo-outdated clippy gcc pkg-config pcsclite xorg.libX11.dev];
+        nativeBuildInputs = with pkgs; [rustc cargo bacon cargo-edit cargo-outdated clippy] ++ buildDependencies;
       };
     });
 }
