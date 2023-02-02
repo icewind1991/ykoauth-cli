0af7a17d7b48e8898d9beff65b7403fc775a8bce
Robin Appelman <robin@icewind.nl>
2023-02-02 20:19:33 +0100

    nix overlay

--- flake.nix
+++ flake.nix
@@ -31,5 +31,26 @@
       devShell = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [rustc cargo bacon cargo-edit cargo-outdated clippy] ++ buildDependencies;
       };
-    });
+    }) // {
+      overlay = final: prev: {
+        ykoauth-cli = final.rustPlatform.buildRustPackage rec {
+          version = "0.1.0";
+          pname = "ykoauth-cli";
+
+          src = ./.;
+
+          cargoSha256 = "sha256-GjRuG/DODkOjYmGqaI7nKRyWRCi91Ne3cZvwbWHO0io=";
+
+          nativeBuildInputs = with final; [gcc pkg-config xorg.libX11.dev];
+          buildInputs = with final; [pcsclite xorg.libX11];
+
+          meta = with final.lib; {
+            description = "CLI for reading TOTP keys from yubikeys";
+            homepage = "https://github.com/icewind1991/ykoauth-cli";
+            license = licenses.mit;
+            platforms = platforms.linux;
+          };
+        };
+      };
+    };
 }
