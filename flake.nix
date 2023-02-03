186c3f9e86e5d215d0abd8490f1fbfbb0c2aedd6
Robin Appelman <robin@icewind.nl>
2023-02-03 20:50:04 +0100

    nix: move away from naersk, opt-out clipboard

--- flake.nix
+++ flake.nix
@@ -2,55 +2,60 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/release-22.11";
-    naersk.url = "github:nix-community/naersk";
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
-    naersk,
-  }:
-    utils.lib.eachDefaultSystem (system: let
+  }: let
+    buildPkg = {
+      rustPlatform,
+      gcc,
+      pkg-config,
+      xorg,
+      pcsclite,
+      lib,
+      enableClipboard ? true,
+    }: let
+      inherit (lib) optionals licenses platforms;
+    in rustPlatform.buildRustPackage rec {
+      version = "0.1.0";
+      pname = "ykoauth-cli";
+
+      src = ./.;
+
+      cargoSha256 = "sha256-fC1PqSm+rjB5zdEKAatlLdEr4SPRn/Txz15kXzs7FLo=";
+
+      nativeBuildInputs = [gcc pkg-config] ++ optionals enableClipboard [xorg.libX11.dev];
+      buildInputs = [pcsclite] ++ optionals enableClipboard [xorg.libX11];
+
+      buildNoDefaultFeatures = true;
+      buildFeatures = optionals enableClipboard ["clipboard"];
+
+      meta = {
+        description = "CLI for reading TOTP keys from yubikeys";
+        homepage = "https://github.com/icewind1991/ykoauth-cli";
+        license = licenses.mit;
+        platforms = platforms.linux;
+      };
+    };
+    in utils.lib.eachDefaultSystem (system: let
       pkgs = (import nixpkgs) {
         inherit system;
       };
-      naersk-lib = naersk.lib."${system}";
-      buildDependencies = with pkgs; [gcc pkg-config pcsclite xorg.libX11.dev];
     in rec {
      # `nix build`
-     packages.ykoauth-cli = naersk-lib.buildPackage {
-       pname = "ykoauth-cli";
-       root = ./.;
-       nativeBuildInputs = buildDependencies;
-     };
+     packages.ykoauth-cli = pkgs.callPackage buildPkg {};
      defaultPackage = packages.ykoauth-cli;
-     defaultApp = packages.ykoauth-cli;
 
       # `nix develop`
       devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [rustc cargo bacon cargo-edit cargo-outdated clippy] ++ buildDependencies;
+        nativeBuildInputs = with pkgs; [rustc cargo bacon cargo-edit cargo-outdated clippy gcc pkg-config pcsclite xorg.libX11.dev];
       };
     }) // {
       overlay = final: prev: {
-        ykoauth-cli = final.rustPlatform.buildRustPackage rec {
-          version = "0.1.0";
-          pname = "ykoauth-cli";
-
-          src = ./.;
-
-          cargoSha256 = "sha256-GjRuG/DODkOjYmGqaI7nKRyWRCi91Ne3cZvwbWHO0io=";
-
-          nativeBuildInputs = with final; [gcc pkg-config xorg.libX11.dev];
-          buildInputs = with final; [pcsclite xorg.libX11];
-
-          meta = with final.lib; {
-            description = "CLI for reading TOTP keys from yubikeys";
-            homepage = "https://github.com/icewind1991/ykoauth-cli";
-            license = licenses.mit;
-            platforms = platforms.linux;
-          };
-        };
+        ykoauth-cli = final.callPackage buildPkg {};
       };
     };
 }
